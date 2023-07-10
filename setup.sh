9ddb0abd98a874dbb12afd8cffb9d0da392ef642
SETANTAZVPN <136411793+SETANTAZVPN@users.noreply.github.com>
2023-07-10 13:09:28 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -187,7 +187,7 @@ echo -e "$green          Install XRAY              $NC"
 echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
 sleep 1
 clear
-wget https://raw.githubusercontent.com/artanodrop/v4/main/xray/ins-xray.sh && chmod +x ins-xray.sh && ./ins-xray.sh
+wget https://raw.githubusercontent.com/SETANTAZVPN/v4/main/xray/ins-xray.sh && chmod +x ins-xray.sh && ./ins-xray.sh
 wget https://raw.githubusercontent.com/SETANTAZVPN/v4/main/Sshws/insshws.sh && chmod +x insshws.sh && ./insshws.sh
 clear
 #pasang rc clone ssh ovpn 
