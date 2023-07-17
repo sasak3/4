7a1953e708e996e362463c7e76cef97033ca0e98
SETANTAZVPN <136411793+SETANTAZVPN@users.noreply.github.com>
2023-07-17 23:07:09 +0700

    Update menu-vless.sh

--- menu/menu-vless.sh
+++ menu/menu-vless.sh
@@ -72,10 +72,10 @@ echo -e "\E[40;1;37m       • MENU VLESS •         \E[0m"
 echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
 echo -e ""
 echo -e " [\e[36m•1\e[0m] Buat Vless "
-echo -e " [\e[36m•2\e[0m] Buat Trial "
-echo -e " [\e[36m•3\e[0m] Ubah Akun Vless "
-echo -e " [\e[36m•4\e[0m] Hapus Akun Vless "
-echo -e " [\e[36m•5\e[0m] Cek User Login Vless "
+#echo -e " [\e[36m•2\e[0m] Buat Trial "
+echo -e " [\e[36m•2\e[0m] Ubah Akun Vless "
+echo -e " [\e[36m•3\e[0m] Hapus Akun Vless "
+echo -e " [\e[36m•4\e[0m] Cek User Login Vless "
 echo -e " [\e[31m•0\e[0m] \e[31mBACK TO MENU\033[0m"
 echo -e ""
 echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
@@ -83,12 +83,17 @@ echo -e ""
 read -p " Select =>  "  opt
 echo -e ""
 case $opt in
-1) clear ; add-vless ; exit ;;
-2) clear ; trialvless ; exit ;;
-3) clear ; renew-vless ; exit ;;
-4) clear ; del-vless ; exit ;;
-5) clear ; cek-vless ; exit ;;
-0) clear ; menu ; exit ;;
+#1) clear ; add-vless ; exit ;;
+#2) clear ; trialvless ; exit ;;
+#3) clear ; renew-vless ; exit ;;
+#4) clear ; del-vless ; exit ;;
+#5) clear ; cek-vless ; exit ;;
+#0) clear ; menu ; exit ;;
+01 | 1) clear ; addvless ;;
+02 | 2) clear ; renewvless ;;
+03 | 3) clear ; delvless ;;
+04 | 4) clear ; cekvless ;;
+00 | 0) clear ; menu ;;
 x) exit ;;
 *) echo "Anda salah tekan " ; sleep 1 ; menu-ssh ;;
 esac
