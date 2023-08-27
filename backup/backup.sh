678236d16982ee27c86c020b1d03b029495b7aa3
SETANTAZVPN <136411793+SETANTAZVPN@users.noreply.github.com>
2023-08-27 15:34:24 +0800

    Update backup.sh

--- backup/backup.sh
+++ backup/backup.sh
@@ -24,14 +24,14 @@ figlet "Backup"
 echo "Mohon Menunggu , Proses Backup sedang berlangsung !!"
 rm -rf /root/backup
 mkdir /root/backup
-cp -r /etc/passwd /backup/
-cp -r /etc/group /backup/
-cp -r /etc/shadow /backup/
-cp -r /etc/gshadow /backup/
-cp -r /etc/xray /backup/xray
-cp -r /root/nsdomain /backup/nsdomain
-cp -r /etc/slowdns /backup/slowdns
-cp -r /home/vps/public_html /backup/public_html
+cp -r /etc/passwd /root/backup/
+cp -r /etc/group /root/backup/
+cp -r /etc/shadow /root/backup/
+cp -r /etc/gshadow /root/backup/
+cp -r /etc/xray /root/backup/xray
+cp -r /root/nsdomain /root/backup/nsdomain
+cp -r /etc/slowdns /root/backup/slowdns
+cp -r /home/vps/public_html /root/backup/public_html
 cd /root
 zip -r $IP-$date.zip backup > /dev/null 2>&1
 rclone copy /root/$IP-$date.zip dr:backup/
