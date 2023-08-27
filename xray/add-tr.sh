5af5fa9c4fbdf8a3060f4e13de6d90c4e463e1e3
SETANTAZVPN <136411793+SETANTAZVPN@users.noreply.github.com>
2023-08-27 09:21:39 +0800

    Update add-tr.sh

--- xray/add-tr.sh
+++ xray/add-tr.sh
@@ -10,6 +10,11 @@ domain=$(cat /etc/xray/domain)
 else
 domain=$IP
 fi
+TIMES="10"
+CHATID=$(cat /etc/id)
+KEY=$(cat /etc/token)
+URL="https://api.telegram.org/bot$KEY/sendMessage"
+
 tr="$(cat ~/log-install.txt | grep -w "Trojan WS " | cut -d: -f2|sed 's/ //g')"
 until [[ $user =~ ^[a-zA-Z0-9_]+$ && ${user_EXISTS} == '0' ]]; do
 echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
@@ -45,6 +50,36 @@ systemctl restart xray
 trojanlink1="trojan://${uuid}@${domain}:${tr}?mode=gun&security=tls&type=grpc&serviceName=trojan-grpc&sni=bug.com#${user}"
 trojanlink="trojan://${uuid}@isi_bug_disini:${tr}?path=%2Ftrojan-ws&security=tls&host=${domain}&type=ws&sni=${domain}#${user}"
 clear
+
+trojan1="$(echo $trojanlink1 | base64 -w 0)"
+trojan2="$(echo $trojanlink | base64 -w 0)"
+
+TEXT="
+<code>◇━━━━━━━━━━━━━━━━━◇</code>
+<code>  Premium Trojan Account</code>
+<code>◇━━━━━━━━━━━━━━━━━◇</code>
+<code>Remarks      : </code> <code>${user}</code>
+<code>Domain       : </code> <code>${domain}</code>
+<code>Port TLS     : </code> <code>443</code>
+<code>Port GRPC    : </code> <code>443</code>
+<code>User ID      : </code> <code>${uuid}</code>
+<code>AlterId      : 0</code>
+<code>Security     : auto</code>
+<code>Network      : WS or gRPC</code>
+<code>Path WS    : </code> <code>/trojan-ws</code>
+<code>Path GRPC  : </code> <code>/trojan-grpc</code>
+<code>Expired On : $exp</code>
+<code>◇━━━━━━━━━━━━━━━━━◇</code>
+<code>Link TLS    :</code> 
+<code>${trojan2}</code>
+<code>◇━━━━━━━━━━━━━━━━━◇</code>
+<code>Link GRPC    :</code> 
+<code>${trojan1}</code>
+<code>◇━━━━━━━━━━━━━━━━━◇</code>
+"
+
+curl -s --max-time $TIMES -d "chat_id=$CHATID&disable_web_page_preview=1&text=$TEXT&parse_mode=html" $URL >/dev/null
+
 echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
 echo -e "\E[40;1;37m        TROJAN ACCOUNT          \E[0m"
 echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
